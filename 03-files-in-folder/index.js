3eb2a2623661953ae0479e6066b3de040b6c2389
User <belpc2012@gmail.com>
2021-11-09 00:51:47 +0300

    feat: 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const { readdir, stat } = require('fs/promises');
+const { join, parse } = require('path');
+const { stdout } = process;
+
+
+async function getFile(pathFrom) {
+  const files = await readdir(pathFrom);
+  if (files.length) {
+    for (let item of files) {
+      const stats = await stat(join(pathFrom, item));
+      if (stats.isFile()) {
+        const name = parse(item).name;
+        const ext = parse(item).ext.slice(1);
+        stdout.write(`${name} - ${ext} - ${stats.size} byte`);
+      }
+    }
+  }
+}
+
+getFile(join(__dirname, 'secret-folder'));
